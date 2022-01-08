366841be01cd53f203f610f13e01214ec75d4c23
hogyun3709 <hogyun3709@gmail.com>
2022-01-09 00:51:30 +0900

    Week1: Feature(UserInput): handle user input text message by a simple rotate method with unit test

--- mission/src/components/HelloWorld.vue
+++ mission/src/components/HelloWorld.vue
@@ -1,14 +1,44 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <user-input type="text" v-model:modelValue="userInputText"></user-input>
+    <p>{{ userInputText }}</p>
+    <button @click="rotateText">제출</button>
+    <button @click="alertMessage">알림</button>
   </div>
 </template>
 
 <script>
+import UserInput from './UserInput.vue';
+
 export default {
   name: 'HelloWorld',
+  components: {
+    'user-input': UserInput,
+  },
   props: {
     msg: String,
+  },
+  data() {
+    return {
+      userInputText: '',
+      alertCounts: 0,
+    };
+  },
+  methods: {
+    rotateText() {
+      /* 선언 순서에 대한 고민 */
+      const extractedChar = this.userInputText.substring(0, 1);
+      const remainedChars = this.userInputText.substring(1);
+
+      this.userInputText = remainedChars + extractedChar;
+    },
+    alertMessage() {
+      this.alertCounts += 1;
+      // eslint의 규칙을 비활성화 해도 되는가?
+      // eslint-disable-next-line
+      alert(`${this.userInputText} ${this.alertCounts}`);
+    },
   },
 };
 </script>
