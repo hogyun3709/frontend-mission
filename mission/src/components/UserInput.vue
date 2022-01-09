8e57c7a2cea403a4941782e1156929bc425cf099
hogyun3709 <hogyun3709@gmail.com>
2022-01-09 19:47:40 +0900

    Week1 Feature(UserInput): to handle user input text message by a simple rotate method with unit test

---
+++ mission/src/components/UserInput.vue
@@ -0,0 +1,16 @@
+<template>
+  <input type="text" v-bind:value="modelValue" v-on:input="handleUserInput" />
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: String,
+  },
+  methods: {
+    handleUserInput(e) {
+      this.$emit('update:modelValue', e.target.value);
+    },
+  },
+};
+</script>
--- mission/tests/unit/example.spec.js
+++ mission/tests/unit/example.spec.js
@@ -1,4 +1,4 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, mount } from '@vue/test-utils';
 import HelloWorld from '@/components/HelloWorld.vue';
 
 describe('HelloWorld.vue', () => {
@@ -9,4 +9,18 @@ describe('HelloWorld.vue', () => {
     });
     expect(wrapper.text()).toMatch(msg);
   });
+  test('checks rotateText method works properly as it designed', async () => {
+    const wrapper = mount(HelloWorld);
+    await wrapper.find('input[type="text"]').setValue('projectlion');
+    await wrapper.find('button').trigger('click');
+    expect(wrapper.find('p').text()).toBe('rojectlionp');
+  });
+  test('checks rotateText method handles IME Issue', async () => {
+    const wrapper = mount(HelloWorld);
+    await wrapper.find('input[type="text"]').setValue('안녕하세요');
+    await wrapper.find('button').trigger('click');
+    expect(wrapper.find('p').text()).toBe('녕하세요안');
+  });
+  /* UserInput.vue 에 대한 unit 테스트 - emitted..? */
+  /* Role of UserInput.vue = as a child component passes data to parent */
 });
