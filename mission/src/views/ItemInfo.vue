5981122f53e1db537a271fd5824d6927ad2e5873
hogyun3709 <hogyun3709@gmail.com>
2022-01-15 14:20:55 +0900

    feat: renders an image with ration of sqaure

--- mission/src/views/ItemInfo.vue
+++ mission/src/views/ItemInfo.vue
@@ -1,24 +1,27 @@
 <template>
-<div id='item-info-page'></div>
+  <div id="item-info-page">
+    <figure class="image is-square">
+      <img data-test="product-image" :src="sellerData.productImg" />
+    </figure>
+  </div>
 </template>
 
 <script>
+import productImage from '@/assets/product-1.png';
+
 export default {
   name: 'ItemInfoPage',
   data() {
     return {
-
+      sellerData: {
+        productImg: productImage,
+      },
     };
   },
-  methods: {
-
-  },
-  computed: {
-
-  },
+  methods: {},
+  computed: {},
 };
 </script>
 
 <style scoped>
-
 </style>
--- mission/tests/unit/ItemInfo/ItemInfo.spec.js
+++ mission/tests/unit/ItemInfo/ItemInfo.spec.js
@@ -2,9 +2,20 @@ import { mount } from '@vue/test-utils';
 import ItemInfoPage from '@/views/ItemInfo.vue';
 
 describe('ItemInfoPage', () => {
-  it('redners ItemInfoPage', () => {
-    const wrapper = mount(ItemInfoPage);
+  let wrapper;
 
+  beforeEach(() => {
+    wrapper = mount(ItemInfoPage);
+  });
+  it('renders ItemInfoPage', () => {
     expect(wrapper.find('#item-info-page').exists()).toBe(true);
+  });
+  /* should check not only element but data - 추후구현 */
+  it('renders seller product image element on DOM', async () => {
+    expect(wrapper.get('img[data-test="product-image"]').exists()).toBe(true);
+  });
+  /* image style height 과 width 의 비율을 1:1 로 확인 할수있는 테스트 코드 구현 필요 */
+  it('checks image has ratio of sqaure', () => {
+    expect(wrapper.find('img[data-test="product-image"]').attributes().style).toBe();
   });
 });
