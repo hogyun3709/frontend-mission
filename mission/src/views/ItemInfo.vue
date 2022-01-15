fa2484e1d78d913b909be99e423c41f64d7a28aa
hogyun3709 <hogyun3709@gmail.com>
2022-01-15 18:35:31 +0900

    feat: renders(display) seller details-profile picture, market name, market tags

--- mission/src/views/ItemInfo.vue
+++ mission/src/views/ItemInfo.vue
@@ -1,8 +1,29 @@
 <template>
-  <div id="item-info-page">
+  <div class="container">
     <figure class="image is-square">
       <img data-test="product-image" :src="sellerData.productImg" />
     </figure>
+    <div class="columns mt-1 mx-1 is-mobile">
+      <div class="column is-one-fifth">
+        <figure class="image is-square">
+          <!-- image 파일에 대한 async loading 이 필요 -->
+          <img
+            class="is-rounded"
+            data-test="profile-picture"
+            :src="sellerData.profilePic"
+          />
+        </figure>
+      </div>
+      <div class="column">
+        <p class="has-text-left is-size-3-mobile" data-test="market-name">
+          {{ sellerData.marketName }}
+        </p>
+        <p class="has-text-left is-size-4-mobile" data-test="seller-tag">
+          {{ sellerData.tags[0] }}{{ sellerData.tags[1] }}
+        </p>
+      </div>
+      <div class="column is-one-fifth"></div>
+    </div>
   </div>
 </template>
 
@@ -15,6 +36,9 @@ export default {
     return {
       sellerData: {
         productImg: productImage,
+        profilePic: 'https://picsum.photos/200/300',
+        marketName: '대한양복',
+        tags: ['#남성', '#수트'],
       },
     };
   },
--- mission/tests/unit/ItemInfo/ItemInfo.spec.js
+++ mission/tests/unit/ItemInfo/ItemInfo.spec.js
@@ -7,9 +7,7 @@ describe('ItemInfoPage', () => {
   beforeEach(() => {
     wrapper = mount(ItemInfoPage);
   });
-  it('renders ItemInfoPage', () => {
-    expect(wrapper.find('#item-info-page').exists()).toBe(true);
-  });
+
   /* should check not only element but data - 추후구현 */
   it('renders seller product image element on DOM', async () => {
     expect(wrapper.get('img[data-test="product-image"]').exists()).toBe(true);
@@ -17,5 +15,19 @@ describe('ItemInfoPage', () => {
   /* image style height 과 width 의 비율을 1:1 로 확인 할수있는 테스트 코드 구현 필요 */
   it('checks image has ratio of sqaure', () => {
     expect(wrapper.find('img[data-test="product-image"]').attributes().style).toBe();
+  });
+  it('renders following elements which will display seller details', () => {
+    expect(wrapper.find('img[data-test="profile-picture"]').exists()).toBe(true);
+    expect(wrapper.find('p[data-test="market-name"]').exists()).toBe(true);
+    expect(wrapper.find('p[data-test="seller-tag"]').exists()).toBe(true);
+  });
+  /* image 가 화면상에 잘 display되는지에 대한 test case 작성 필요 */
+  it('displays seller infos from data', () => {
+    const testSellerName = '대한양복';
+    const testSellerTag = '#남성#수트';
+    // const testSellerProfilePic = 'https://picsum.photos/200/300';
+    expect(wrapper.get('p[data-test="market-name"]').text()).toEqual(testSellerName);
+    expect(wrapper.get('p[data-test="seller-tag"]').text()).toEqual(testSellerTag);
+    // expect(wrapper.get('img[data-test="profile-picture"]').text()).toEqual(testSellerProfilePic);
   });
 });
