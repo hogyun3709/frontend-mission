1623b300056b06a8f29b748d134826b3af78946f
hogyun3709 <hogyun3709@gmail.com>
2022-01-20 14:51:52 +0900

    Edit: Header TC update

--- mission/tests/unit/Header.spec.js
+++ mission/tests/unit/Header.spec.js
@@ -2,21 +2,17 @@ import { mount } from '@vue/test-utils';
 import Header from '@/views/Header.vue';
 
 describe('Header', () => {
-	it('renders Header', () => {
-		const wrapper = mount(Header);
+	let wrapper;
+
+	beforeEach(() => {
+		wrapper = mount(Header);
+	});
+	it('renders Header elemnt', () => {
 		expect(wrapper.find('div[data-test="header"]').exists()).toBe(true);
 	});
+	it('checks shopping mall title text display', () => {
+		const testShoppingMallName = 'Test Shopping Mall'
 
-	it('displays name of the mall', () => {
-		const testShoppingMallName = 'Test Shopping Mall';
-
-		const wrapper = mount(Header, {
-			data() {
-				return {
-					shoppingMallName: testShoppingMallName,
-				};
-			},
-		});
 		expect(wrapper.get('div[data-test="header"]').text()).toBe(testShoppingMallName);
-	});
+	})
 });
