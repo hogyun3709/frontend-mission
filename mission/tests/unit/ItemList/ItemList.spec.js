0b72312f60f32d75a84f24508de8204ad4543ce6
hogyun3709 <hogyun3709@gmail.com>
2022-01-21 12:54:32 +0900

    test: checks rendering item component according to number of item data(props)

--- mission/tests/unit/ItemList/ItemList.spec.js
+++ mission/tests/unit/ItemList/ItemList.spec.js
@@ -1,10 +1,43 @@
 import { mount } from '@vue/test-utils';
-import ItemInfoPage from '@/views/ItemList.vue';
+import ItemListPage from '@/views/ItemList.vue';
+
+import Item from '@/components/ItemList/Item.vue';
 
 describe('ItemListPage', () => {
-  it('redners ItemListPage', () => {
-    const wrapper = mount(ItemInfoPage);
 
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(ItemListPage)
+  })
+
+  it('renders ItemListPage', () => {
     expect(wrapper.find('#item-list-page').exists()).toBe(true);
   });
+
+  it('props로 건내줄 item data가 없으면, Item 컴포넌트를 렌더링 하지 않음을 테스트 합니다.', async () => {
+    const items = []
+    await wrapper.setData({
+      items,
+    });
+    expect(wrapper.findComponent(Item).exists()).toBe(false)
+  })
+
+  it('props(data)가 1개 이상 있다면, 컴포넌트도 1개 이상 렌더링함을 테스트 합니다.', async () => {
+    const items = [{ id: 1 }, { id: 2 }]
+    await wrapper.setData({
+      items,
+    })
+    const components = wrapper.findAllComponents(Item);
+
+    expect(components.length).toBeGreaterThanOrEqual(1)
+  })
+
+  it('렌더링 된 item 컴포넌트가 없다면(0개), 실패함을 테스트 합니다.', async () => {
+    const items = [{ id: 1 }]
+    await wrapper.setData({
+      items,
+    })
+    expect(wrapper.getComponent(Item)).toThrowError
+  })
 });
