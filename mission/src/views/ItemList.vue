18dd26f920683163f4e4412699b6624c56e4b51d
hogyun3709 <hogyun3709@gmail.com>
2022-01-22 12:45:45 +0900

    style % edit: stlying with data expansion

--- mission/src/views/ItemList.vue
+++ mission/src/views/ItemList.vue
@@ -1,6 +1,11 @@
 <template>
-  <div id="item-list-page">
-    <Item v-for="item in items" :item="item" :key="item.id" />
+  <div class="container mb-5 pb-5">
+    <Item
+      v-for="item in items"
+      :item="item"
+      :key="item.id"
+      class="item mx-3 my-2"
+    />
   </div>
 </template>
 
@@ -19,25 +24,55 @@ export default {
           id: 1,
           img: 'https://picsum.photos/200',
           title: 'Item 1',
-          isDiscount: false,
-          dicountRate: 0,
+          isDiscount: true,
+          discountRate: 10,
           originalPrice: 200000,
+          discription: '제품에 대한 설명입니다',
         },
         {
           id: 2,
           img: 'https://picsum.photos/200',
           title: 'Item 2',
           isDiscount: false,
-          dicountRate: 0,
+          discountRate: 0,
           originalPrice: 200000,
+          discription: '제품에 대한 설명입니다',
         },
         {
           id: 3,
           img: 'https://picsum.photos/200',
           title: 'Item 3',
           isDiscount: false,
-          dicountRate: 0,
+          discountRate: 0,
           originalPrice: 200000,
+          discription: '제품에 대한 설명입니다',
+        },
+        {
+          id: 4,
+          img: 'https://picsum.photos/200',
+          title: 'Item 4',
+          isDiscount: false,
+          discountRate: 0,
+          originalPrice: 200000,
+          discription: '제품에 대한 설명입니다',
+        },
+        {
+          id: 5,
+          img: 'https://picsum.photos/200',
+          title: 'Item 4',
+          isDiscount: false,
+          discountRate: 0,
+          originalPrice: 200000,
+          discription: '제품에 대한 설명입니다',
+        },
+        {
+          id: 6,
+          img: 'https://picsum.photos/200',
+          title: 'Item 4',
+          isDiscount: false,
+          discountRate: 0,
+          originalPrice: 200000,
+          discription: '제품에 대한 설명입니다',
         },
       ],
     };
@@ -46,4 +81,14 @@ export default {
 </script>
 
 <style>
+.container {
+  display: flex;
+  flex-wrap: wrap;
+}
+.item {
+  width: 100%;
+}
+.container > div {
+  flex: 40%;
+}
 </style>
