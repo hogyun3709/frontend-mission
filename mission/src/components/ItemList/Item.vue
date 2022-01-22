f9cf09b9423ed3e27b6f55efe62a1af667703694
hogyun3709 <hogyun3709@gmail.com>
2022-01-22 12:44:23 +0900

    feat: rendering item props data

--- mission/src/components/ItemList/Item.vue
+++ mission/src/components/ItemList/Item.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="item-list-item">This is Item Component</div>
+  <div class="has-text-left">
+    <img :src="item.img" />
+    <div class="has-text-weight-bold">
+      <span
+        v-show="item.isDiscount"
+        :class="{ 'has-text-danger': item.isDiscount }"
+      >
+        {{ discountRate }}
+      </span>
+      {{ finalPrice }}
+    </div>
+    <h4>{{ item.title }}</h4>
+    <div class="is-size-7">{{ item.discription }}</div>
+  </div>
 </template>
 
 <script>
@@ -7,6 +20,20 @@ export default {
   name: 'ItemListItem',
   props: {
     item: Object,
+  },
+  computed: {
+    finalPrice() {
+      const price = this.item.originalPrice;
+      const rate = this.item.discountRate;
+      const discount = this.item.isDiscount;
+      const discountedPrice = price - price * (rate / 100);
+      return discount
+        ? `${discountedPrice.toLocaleString()}원`
+        : `${price.toLocaleString()}원`;
+    },
+    discountRate() {
+      return `${this.item.discountRate}%`;
+    },
   },
 };
 </script>
