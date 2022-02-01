c46358d177e10c399e6c041640dcc576412f74e0
hogyun3709 <hogyun3709@gmail.com>
2022-02-01 11:28:17 +0900

    Fix: props validation 추가로 TC의 undefined 에러 해결

--- mission/src/components/ItemList/Item.vue
+++ mission/src/components/ItemList/Item.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="has-text-left">
-    <img data-test="item-img" :src="item.img" />
+    <img data-test="item-img" :src="img" />
     <div class="has-text-weight-bold">
       <span
-        v-show="item.isDiscount"
-        :class="{ 'has-text-danger': item.isDiscount }"
+        v-show="isDiscount"
+        :class="{ 'has-text-danger': isDiscount }"
         data-test="discount-rate"
       >
         {{ discountRate }}
       </span>
       <span data-test="final-price">{{ finalPrice }}</span>
     </div>
-    <h4 data-test="item-title">{{ item.title }}</h4>
+    <h4 data-test="item-title">{{ title }}</h4>
     <div data-test="item-discription" class="is-size-7">
-      {{ item.discription }}
+      {{ discription }}
     </div>
   </div>
 </template>
@@ -22,20 +22,54 @@
 export default {
   name: 'ItemListItem',
   props: {
-    item: Object,
+    id: {
+      type: Number,
+      default: 1,
+      required: true,
+    },
+    img: {
+      type: String,
+      default: 'https://picsum.photos/200',
+      required: true,
+    },
+    title: {
+      type: String,
+      default: '',
+      required: true,
+    },
+    isDiscount: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+    discountRate: {
+      type: Number,
+      default: null,
+      required: true,
+    },
+    originalPrice: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+    discription: {
+      type: String,
+      default: '',
+      required: true,
+    },
   },
   computed: {
     finalPrice() {
-      const price = this.item.originalPrice;
-      const rate = this.item.discountRate;
-      const discount = this.item.isDiscount;
+      const price = this.originalPrice;
+      const rate = this.discountRate;
+      const discount = this.isDiscount;
       const discountedPrice = price - price * (rate / 100);
       return discount
         ? `${discountedPrice.toLocaleString()}원`
         : `${price.toLocaleString()}원`;
     },
     discountRate() {
-      return `${this.item.discountRate}%`;
+      return `${this.discountRate}%`;
     },
   },
 };
