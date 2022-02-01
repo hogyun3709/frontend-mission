2d1bd3d15eb018429df7cbd25092db65ea955bb7
hogyun3709 <hogyun3709@gmail.com>
2022-02-02 00:51:54 +0900

    Edit: 3주차 props validation 추가 적용

--- mission/src/views/ItemList.vue
+++ mission/src/views/ItemList.vue
@@ -2,7 +2,13 @@
   <div class="container mb-5 pb-5" data-test="item-list-page">
     <Item
       v-for="item in items"
-      :item="item"
+      :id="item.id"
+      :img="item.img"
+      :title="item.title"
+      :isDiscount="item.isDiscount"
+      :discount_rate="item.discount_rate"
+      :original_price="item.original_price"
+      :discription="item.discription"
       :key="item.id"
       class="item mx-3 my-2"
     />
@@ -25,8 +31,8 @@ export default {
           img: 'https://picsum.photos/200',
           title: 'Item 1',
           isDiscount: true,
-          discountRate: 10,
-          originalPrice: 200000,
+          discount_rate: 10,
+          original_price: 200000,
           discription: '제품에 대한 설명입니다',
         },
         {
@@ -34,8 +40,8 @@ export default {
           img: 'https://picsum.photos/200',
           title: 'Item 2',
           isDiscount: false,
-          discountRate: 0,
-          originalPrice: 200000,
+          discount_rate: 0,
+          original_price: 200000,
           discription: '제품에 대한 설명입니다',
         },
         {
@@ -43,8 +49,8 @@ export default {
           img: 'https://picsum.photos/200',
           title: 'Item 3',
           isDiscount: false,
-          discountRate: 0,
-          originalPrice: 200000,
+          discount_rate: 0,
+          original_price: 200000,
           discription: '제품에 대한 설명입니다',
         },
         {
@@ -52,8 +58,8 @@ export default {
           img: 'https://picsum.photos/200',
           title: 'Item 4',
           isDiscount: false,
-          discountRate: 0,
-          originalPrice: 200000,
+          discount_rate: 0,
+          original_price: 200000,
           discription: '제품에 대한 설명입니다',
         },
         {
@@ -61,8 +67,8 @@ export default {
           img: 'https://picsum.photos/200',
           title: 'Item 4',
           isDiscount: false,
-          discountRate: 0,
-          originalPrice: 200000,
+          discount_rate: 0,
+          original_price: 200000,
           discription: '제품에 대한 설명입니다',
         },
         {
@@ -70,8 +76,8 @@ export default {
           img: 'https://picsum.photos/200',
           title: 'Item 4',
           isDiscount: false,
-          discountRate: 0,
-          originalPrice: 200000,
+          discount_rate: 0,
+          original_price: 200000,
           discription: '제품에 대한 설명입니다',
         },
       ],
