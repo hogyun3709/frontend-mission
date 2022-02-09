b153e46aa7bf3b30a8d0ccc5800aa9467f3b1898
hogyun3709 <hogyun3709@gmail.com>
2022-02-09 14:56:58 +0900

    Refactoring: 변경된 api 구조 반영후 데이터 렌더링

--- mission/src/views/ItemList.vue
+++ mission/src/views/ItemList.vue
@@ -1,40 +1,46 @@
 <template>
   <div class="container mb-5 pb-5" data-test="item-list-page">
-    <Header />
-    <Item v-for="item in items" :key="item.id" :product="item" />
-    <BottomNav />
+    <router-link
+      v-for="item in items"
+      :to="itemNoPath(item.product_no)"
+      :key="item.product_no"
+    >
+      <Item
+        :name="item.name"
+        :description="item.description"
+        :img="item.image"
+        :price="item.price"
+        :original_price="item.original_price"
+        :key="item.product_no"
+      ></Item>
+    </router-link>
   </div>
 </template>
 
 <script>
-import Header from '@/views/Header.vue';
-import BottomNav from '@/views/BottomNav.vue';
 import Item from '@/components/ItemList/Item.vue';
-import Repository from '@/repositories/RepositoryFactory';
-
-const ItemRepository = Repository.get('item');
+import ItemApi from '@/api/Item/ItemApi.js';
 
 export default {
   name: 'ItemListPage',
   components: {
     Item,
-    Header,
-    BottomNav,
   },
   data() {
     return {
-      items: {},
+      items: [],
     };
   },
   methods: {
-    async getItemLists() {
-      const { data } = await ItemRepository.get();
-      this.items = data;
-      console.log(this.items);
+    itemNoPath(productNo) {
+      return `/item/${productNo}`;
     },
   },
-  created() {
-    this.getItemLists();
+  async created() {
+    const apiClient = new ItemApi();
+    const response = await apiClient.getItemList();
+    this.items = response.data.items;
+    console.log(this.items);
   },
 };
 </script>
